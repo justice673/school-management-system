e13e23cec4be262d20179927606c5168aacd8fb4
justice673 <fongejustice918@gmail.com>
2024-11-12 00:49:11 +0100

    done

--- src/components/ConfirmationEditModal/ConfirmationEditModal.jsx
+++ src/components/ConfirmationEditModal/ConfirmationEditModal.jsx
@@ -8,7 +8,7 @@ const ConfirmationEditModal = ({ isOpen, onClose, onConfirm }) => {
         <div className={styles.overlay}>
             <div className={styles.modal}>
                 <h2>Are you sure?</h2>
-                <p>Do you want to edit this student's details? </p>
+                <p>Do you want to edit this student&#39;s details? </p>
                 <div className={styles.buttons}>
                     <button onClick={onClose} className={styles.cancelButton}>Cancel</button>
                     <button onClick={onConfirm} className={styles.confirmButton}>Edit</button>
